02d3dba86ae08a3700267bc883669fa549dea5a8
Josh Campbell <runguy@ymail.com>
2014-03-07 15:01:10 -0500

    fixed the cyclic barrier.

--- sim/concurrent/barrier.cpp
+++ sim/concurrent/barrier.cpp
@@ -5,6 +5,10 @@
 #include "barrier.h"
 
 /**
+ * Implementation of a cyclic non-spinning barrier.
+ */
+
+/**
  * Constructor.
  */
 Barrier::Barrier(int total)
@@ -25,18 +29,21 @@ void Barrier::arrive()
     std::unique_lock<std::mutex> lock(mutex);
 
     arrived++;
+    int old_r = release;
 
     if (arrived == total)
     {
-        arrived = 0;
         // TODO Delete. Creates time between steps.
         std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        arrived = 0;
+        release = (release + 1) % 2;
         condition.notify_all();
     }
     else
     {
         // Block all spurious wakeups.
         // TODO fix wait condition.
-        condition.wait(lock, [=]{return false;});
+        condition.wait(lock, [=]{return old_r != release;});
     }
 }
--- sim/concurrent/barrier.h
+++ sim/concurrent/barrier.h
@@ -5,7 +5,7 @@
 #include <mutex>
 
 /**
- * A barrier class. 
+ * A cyclic barrier.
  */
 class Barrier
 {
@@ -15,10 +15,14 @@ class Barrier
 
         void arrive();
     private:
+        // Used when entering the barrier.
         int total;
         int arrived = 0;
-
+        // Used to prevent thread from leaving the barrier.
+        int release = 0;
+        // Lock on this mutex.
         std::mutex mutex;
+        // Wait on this condition variable.
         std::condition_variable condition;
 };
 
